3d3566f43458927f5175708b3052196ae9923905
Frontend-Umidjon <umidjonkengashev6@gamil.com>
2024-11-25 10:10:12 +0500

    uy ishi

--- codewars.js
+++ codewars.js
@@ -21,8 +21,46 @@
 // 3-masala
 
 {
-    // array sonlarni teskari qilib qaytaradigan funksiya
+    // raqamni  teskari array qilib qaytaradigan funksiya
     function digitize(n) {
         return Array.from(String(n), Number).reverse()
     }
+}
+
+// 4-masala
+
+{
+// bu masalada bir odam soatiga 0.5 litr suv ichar ekan funksiyada soat kiritilsa shu vaqtni ichida qancha suv ichganini butun aniqlaydigan funksiya
+function litres(time) {
+    return Math.floor(0.5 * time)
+  }
+}
+
+// 5-masala
+
+{
+    // ikkita kiritilgan qiymatlar ustidan odiiy matematik amallar bajaradigan funksiya
+
+    function basicOp(operation, value1, value2){
+        if (operation === "+"){
+           return value1  + value2
+        }else if (operation === "-"){
+          return value1 - value2
+        }else if (operation === "*"){
+          return value1 * value2
+        }
+        else if (operation === "/"){
+          return value1 / value2
+        }
+      }
+}
+
+// 6-masala
+
+{
+    // boolean qiyamtlarni stringa o'giradigan funksiya
+
+    function booleanToString(b){
+        return b.toString();
+       }
 }
